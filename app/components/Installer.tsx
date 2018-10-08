1823de4cc3d0c02a0e3a90585f15bbd8a4e2aa32
itowlson <github@hestia.cc>
2018-10-08 18:25:10 +1300

    Made some of the form handling more generic

--- app/components/Installer.tsx
+++ app/components/Installer.tsx
@@ -1,38 +1,57 @@
 import * as React from 'react';
-import { Button, Container, Form, Header, Input, InputOnChangeData, Segment } from 'semantic-ui-react';
+import { Button, Container, Form, Header, Input, InputOnChangeData, Segment, Label } from 'semantic-ui-react';
 
 import { Actionable } from './contract';
+import { parseParameters, ParameterDefinition } from '../utils/parameters';
+import { hasCredentials } from '../utils/credentials';
+
+const bundle = require('../../data/bundle.json');
 
 interface Properties {
   readonly parent: React.Component<any, Actionable, any>;
 }
 
 interface State {
-  port: number;
+  parameterValues: { [key: string]: string };
 }
 
 export default class Installer extends React.Component<Properties, State, {}>  {
+  private readonly parameterDefinitions: ParameterDefinition[];
+  private readonly hasCredentials: boolean;
+
   constructor(props: Readonly<Properties>) {
     super(props);
-    this.state = { port: 8080 };
+
+    this.parameterDefinitions = parseParameters(bundle)
+    this.hasCredentials = hasCredentials(bundle);
+
+    const initialParameterValues = this.parameterDefinitions.map((pd) => ({ [pd.name]: (pd.defaultValue || '').toString() }));
+    const ipvObj: { [key: string]: string } = Object.assign({}, ...initialParameterValues);
+    this.state = { parameterValues: ipvObj };
 
     this.handleInputChange = this.handleInputChange.bind(this);
-    //this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   private handleInputChange(e: any, c: InputOnChangeData & {name: keyof State}) {
-    console.log(e);
-    console.log(c);
-    this.setState({ [c.name]: Number.parseInt(c.value) });
+    const parameterValues = Object.assign({}, this.state.parameterValues, { [c.name]: c.value });
+    this.setState({ parameterValues: parameterValues });
   }
 
   render() {
+    const credentialsUI = this.hasCredentials ? (<Label>You need credentials</Label>) : (<Label>This bundle does not require any credentials</Label>);
+    const parameterUIs = // this.parameterDefinitions.map((pd) => inputWidget(pd));
+      [
+        (<Form.Field key="port" name="port" control={Input} label="Port" type="number" value={this.state.parameterValues.port} onChange={this.handleInputChange} />),
+        (<Form.Field key="favuriteWomble" name="favouriteWomble" control={Input} label="Favourite Womble" type="string" value={this.state.parameterValues.favouriteWomble} onChange={this.handleInputChange} />)
+      ]
+
     return (
       <Container>
         <Segment raised>
           <Header sub>Installation parameters</Header>
           <Form>
-            <Form.Field name="port" control={Input} label="Port" type="number" value={this.state.port} onChange={this.handleInputChange} />
+            {...parameterUIs}
+            {credentialsUI}
           </Form>
           <Button onClick={() => this.install()}>Install</Button>
         </Segment>
@@ -41,7 +60,9 @@ export default class Installer extends React.Component<Properties, State, {}>  {
   }
 
   private async install(): Promise<void> {
-    console.log(this.state.port);
+    for (const k of Object.keys(this.state.parameterValues)) {
+      console.log(`${k}=${this.state.parameterValues[k]}`);
+    }
     // const sr = await shell.exec('d:\\GoProjects\\src\\github.com\\deis\\duffle\\bin\\duffle.exe version');
     // if (succeeded(sr)) {
     //   if (sr.result.code === 0) {
---
+++ app/utils/parameters.ts
@@ -0,0 +1,18 @@
+export interface ParameterDefinition {
+    readonly name: string;
+    readonly type: string;
+    readonly allowedValues?: number[] | string[] | boolean[];
+    readonly defaultValue?: number | string | boolean;
+    readonly metadata?: { description?: string };
+}
+
+export function parseParameters(json: any): ParameterDefinition[] {
+    const parameters = json.parameters;
+    const defs: ParameterDefinition[] = [];
+    if (parameters) {
+        for (const k in parameters) {
+            defs.push({ name: k, ...parameters[k] });
+        }
+    }
+    return defs;
+}

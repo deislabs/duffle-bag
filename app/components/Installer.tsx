2e56bba467eff3a5e6fe0f1de306c5c0c169f3d5
itowlson <github@hestia.cc>
2018-10-08 13:49:10 +1300

    Epic battle to enter a (hardwired) parameter. This should be easier!

--- app/components/Installer.tsx
+++ app/components/Installer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Container, Header, Segment } from 'semantic-ui-react';
+import { Button, Container, Form, Header, Input, InputOnChangeData, Segment } from 'semantic-ui-react';
 
 import { Actionable } from './contract';
 
@@ -8,34 +8,49 @@ interface Properties {
 }
 
 interface State {
+  port: number;
 }
 
 export default class Installer extends React.Component<Properties, State, {}>  {
   constructor(props: Readonly<Properties>) {
     super(props);
-    this.state = {};
+    this.state = { port: 8080 };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  private handleInputChange(e: any, c: InputOnChangeData & {name: keyof State}) {
+    console.log(e);
+    console.log(c);
+    this.setState({ [c.name]: Number.parseInt(c.value) });
   }
 
   render() {
     return (
       <Container>
         <Segment raised>
-          <Header sub>Installing all the things</Header>
+          <Header sub>Installation parameters</Header>
+          <Form>
+            <Form.Field name="port" control={Input} label="Port" type="number" value={this.state.port} onChange={this.handleInputChange} />
+          </Form>
+          <Button onClick={() => this.install()}>Install</Button>
         </Segment>
       </Container>
     );
   }
 
-  // private async install(): Promise<void> {
-  //   const sr = await shell.exec('d:\\GoProjects\\src\\github.com\\deis\\duffle\\bin\\duffle.exe version');
-  //   if (succeeded(sr)) {
-  //     if (sr.result.code === 0) {
-  //       this.setState({ version: sr.result.stdout.trim(), error: null });
-  //     } else {
-  //       this.setState({ version: null, error: sr.result.stderr.trim() });
-  //     }
-  //   } else {
-  //     this.setState({ version: null, error: sr.error[0] });
-  //   }
-  // }
+  private async install(): Promise<void> {
+    console.log(this.state.port);
+    // const sr = await shell.exec('d:\\GoProjects\\src\\github.com\\deis\\duffle\\bin\\duffle.exe version');
+    // if (succeeded(sr)) {
+    //   if (sr.result.code === 0) {
+    //     this.setState({ version: sr.result.stdout.trim(), error: null });
+    //   } else {
+    //     this.setState({ version: null, error: sr.result.stderr.trim() });
+    //   }
+    // } else {
+    //   this.setState({ version: null, error: sr.error[0] });
+    // }
+  }
 }
